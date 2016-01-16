fcb2116c92d2453c80c0e0068c6fa9bd8ab95c9f
Kirurobo <kirurobo@gmail.com>
2016-01-16 22:27:32 +0900

    単純にカラー画像の表示を行う。

---
+++ OpenCvKinectV2/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace OpenCvKinectV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxColorFrame = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxColorFrame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxColorFrame
+            // 
+            this.pictureBoxColorFrame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxColorFrame.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxColorFrame.Name = "pictureBoxColorFrame";
+            this.pictureBoxColorFrame.Size = new System.Drawing.Size(624, 321);
+            this.pictureBoxColorFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxColorFrame.TabIndex = 0;
+            this.pictureBoxColorFrame.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 321);
+            this.Controls.Add(this.pictureBoxColorFrame);
+            this.Name = "Form1";
+            this.Text = "OpenCvSharp3 & Kinect v2";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxColorFrame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxColorFrame;
+    }
+}
+
---
+++ OpenCvKinectV2/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace OpenCvKinectV2
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// KinectSensorのインスタンス
+        /// </summary>
+        KinectSensor kinect;
+
+        /// <summary>
+        /// 最新のカラー画像
+        /// </summary>
+        Mat colorImage;
+
+        /// <summary>
+        /// PictureBoxで表示するビットマップ
+        /// </summary>
+        Bitmap colorBitmap;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ウィンドウが開いたときの処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // KinectSensorのインスタンスを取得
+            this.kinect = KinectSensor.GetDefault();
+
+            // カラー、深度等をまとめて取得するイベントハンドラを設定
+            MultiSourceFrameReader multiSourceFrameReader = this.kinect.OpenMultiSourceFrameReader(
+                FrameSourceTypes.Color      // 今回はカラーのみ
+                );
+            multiSourceFrameReader.MultiSourceFrameArrived += FrameArrived;
+
+            // 画像の準備
+            InitializeImage();
+
+            // Kinect利用開始
+            this.kinect.Open();
+        }
+
+        /// <summary>
+        /// ウィンドウが閉じられたときの処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Kinect利用終了
+            this.kinect.Close();
+        }
+
+        /// <summary>
+        /// 画像を新規作成
+        /// </summary>
+        private void InitializeImage()
+        {
+            // カラー画像の準備
+            FrameDescription colorFrameDescription = this.kinect.ColorFrameSource.FrameDescription;
+            this.colorImage = new Mat(
+                colorFrameDescription.Height,
+                colorFrameDescription.Width,
+                MatType.CV_8UC4                     // 8ビット×4チャンネル分
+                );
+            this.colorImage.SetTo(Scalar.All(255)); // 画像全体を白色に塗りつぶし
+
+            // カラー画像Bitmapの作成とPictureBoxへの割り当て
+            this.colorBitmap = this.colorImage.ToBitmap();
+            this.pictureBoxColorFrame.Image = this.colorBitmap;
+        }
+
+        /// <summary>
+        /// Kinectのデータ取得時に呼ばれる処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            MultiSourceFrame multiSourceFrame = e.FrameReference.AcquireFrame();
+
+            // カラー画像取得時の処理
+            using (ColorFrame colorFrame = multiSourceFrame.ColorFrameReference.AcquireFrame())
+            {
+                if (colorFrame != null)
+                {
+                    // OpenCVの画像にKinectのカラー画像を複製
+                    colorFrame.CopyConvertedFrameDataToIntPtr(
+                        this.colorImage.Data,   // カラー画像Matのデータ部分ポインタ
+                        (uint)(this.colorImage.Total() * this.colorImage.ElemSize()),   // 全画素数 × 一画素のバイト数
+                        ColorImageFormat.Bgra   // RGBでなくBGRAの順番
+                        );
+
+                    // カラー画像からPictureBoxへの描画
+                    DrawColorImage();
+                }
+            }
+        }
+
+        /// <summary>
+        /// PictureBoxに表示させる
+        /// </summary>
+        private void DrawColorImage()
+        {
+            // 画像からビットマップに上書き
+            this.colorImage.ToBitmap(this.colorBitmap);
+
+            // PictureBoxの描画を要求
+            this.pictureBoxColorFrame.Invalidate();
+        }
+    }
+}
