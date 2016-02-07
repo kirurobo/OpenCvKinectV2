4e73c6724243cecf764e6ed8e6a7ab523f87c583
Kirurobo <kirurobo@gmail.com>
2016-02-07 16:38:33 +0900

    深度画像も表示するようにした。

--- OpenCvKinectV2/Form1.Designer.cs
+++ OpenCvKinectV2/Form1.Designer.cs
@@ -28,38 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBoxColor = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDepth = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxColor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDepth)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pictureBoxColor
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(624, 321);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxColor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxColor.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxColor.Name = "pictureBoxColor";
+            this.pictureBoxColor.Size = new System.Drawing.Size(356, 321);
+            this.pictureBoxColor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxColor.TabIndex = 0;
+            this.pictureBoxColor.TabStop = false;
+            // 
+            // pictureBoxDepth
+            // 
+            this.pictureBoxDepth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxDepth.Location = new System.Drawing.Point(362, 0);
+            this.pictureBoxDepth.Name = "pictureBoxDepth";
+            this.pictureBoxDepth.Size = new System.Drawing.Size(261, 321);
+            this.pictureBoxDepth.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxDepth.TabIndex = 1;
+            this.pictureBoxDepth.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 321);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBoxDepth);
+            this.Controls.Add(this.pictureBoxColor);
             this.Name = "Form1";
             this.Text = "OpenCvSharp3 & Kinect v2";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxColor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDepth)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxColor;
+        private System.Windows.Forms.PictureBox pictureBoxDepth;
     }
 }
 
--- OpenCvKinectV2/Form1.cs
+++ OpenCvKinectV2/Form1.cs
@@ -20,14 +20,29 @@ namespace OpenCvKinectV2
         Mat colorImage;
 
         /// <summary>
+        /// 最新の深度画像
+        /// </summary>
+        Mat depthImage;
+
+        /// <summary>
         /// 出力用に処理後の画像
         /// </summary>
         Mat colorOutputImage;
 
         /// <summary>
-        /// PictureBoxで表示するBitmap
+        /// 出力用に処理後の画像
+        /// </summary>
+        Mat depthOutputImage;
+
+        /// <summary>
+        /// カラー画像を入れるBitmap
         /// </summary>
         Bitmap colorBitmap;
+
+        /// <summary>
+        /// 深度画像を入れるBitmap
+        /// </summary>
+        Bitmap depthBitmap;
 
 
         public Form1()
@@ -47,7 +62,7 @@ namespace OpenCvKinectV2
 
             // カラー、深度等をまとめて取得するイベントハンドラを設定
             MultiSourceFrameReader multiSourceFrameReader = this.kinect.OpenMultiSourceFrameReader(
-                FrameSourceTypes.Color      // 今回はカラーのみ
+                FrameSourceTypes.Color | FrameSourceTypes.Depth     // カラーと深度
                 );
             multiSourceFrameReader.MultiSourceFrameArrived += FrameArrived;
 
@@ -75,11 +90,31 @@ namespace OpenCvKinectV2
             // 同じサイズ・深度で出力用画像を作成
             this.colorOutputImage = this.colorImage.Clone();
 
-            // カラー画像Bitmapの作成
+            // カラー画像Bitmapの作成とPictureBosへの割り当て
             this.colorBitmap = this.colorOutputImage.ToBitmap();
+            this.pictureBoxColor.Image = this.colorBitmap;
 
-            // PictureBoxへBitmapを割り当て
-            this.pictureBox1.Image = this.colorBitmap;
+
+            // 深度画像の準備
+            FrameDescription depthFrameDescription = this.kinect.DepthFrameSource.FrameDescription;
+            this.depthImage = new Mat(
+                depthFrameDescription.Height,
+                depthFrameDescription.Width,
+                MatType.CV_16UC1                     // 16ビット×1チャンネル
+                );
+            this.depthImage.SetTo(Scalar.All(0));    // 画像全体を黒（ゼロ）に塗りつぶし
+
+            // 表示用深度画像の準備。深度の値は直接表示に向かないため。
+            this.depthOutputImage = new Mat(
+                depthFrameDescription.Height,
+                depthFrameDescription.Width,
+                MatType.CV_8UC1                     // 8ビット×1チャンネル
+                );
+            this.depthOutputImage.SetTo(Scalar.All(0));    // 画像全体を黒（ゼロ）に塗りつぶし
+
+            // 深度画像Bitmapの作成とPictureBoxへの割り当て
+            this.depthBitmap = this.depthOutputImage.ToBitmap();
+            this.pictureBoxDepth.Image = this.depthBitmap;
         }
 
         /// <summary>
@@ -108,13 +143,32 @@ namespace OpenCvKinectV2
                     Cv2.Threshold(this.colorImage, this.colorOutputImage, 127.0, 255.0, ThresholdTypes.Binary);
 
                     // PictureBoxへ描画
-                    UpdatePictureBox(this.colorOutputImage);
+                    UpdatePictureBox(this.colorOutputImage);    // this.colorImage を渡すと元のカラー画像が見られる
+                }
+            }
+
+            // 深度画像取得時の処理
+            using (DepthFrame depthFrame = multiSourceFrame.DepthFrameReference.AcquireFrame())
+            {
+                if (depthFrame != null)
+                {
+                    // OpenCVの画像にKinectの深度画像を複製
+                    depthFrame.CopyFrameDataToIntPtr(
+                        this.depthImage.Data,
+                        (uint)(this.depthImage.Total() * this.depthImage.ElemSize())
+                        );
+
+                    // 0mm～8000mmの16ビット深度画像を256階調に変換
+                    this.depthImage.ConvertTo(this.depthOutputImage, this.depthOutputImage.Type(), 255.0 / 8000.0);
+
+                    // 深度画像からPictureBoxへの描画
+                    UpdateDepthPictureBox(this.depthOutputImage);   // ここでは this.depthImage は渡せない。色深度が異なる。
                 }
             }
         }
 
         /// <summary>
-        /// PictureBoxの表示を更新
+        /// カラー表示PictureBoxの表示を更新
         /// <param name="image">表示画像。colorBitmapと同じサイズ・色深度でなければならない</param>
         /// </summary>
         private void UpdatePictureBox(Mat image)
@@ -123,7 +177,21 @@ namespace OpenCvKinectV2
             image.ToBitmap(this.colorBitmap);
 
             // PictureBoxの描画を要求
-            this.pictureBox1.Invalidate();
+            this.pictureBoxColor.Invalidate();
+        }
+
+        /// <summary>
+        /// 深度表示PictureBoxを更新
+        /// <param name="image">表示画像。depthBitmapと同じサイズ・色深度でなければならない
+        /// Kinectから得られる16bitの深度ではないので注意。</param>
+        /// </summary>
+        private void UpdateDepthPictureBox(Mat image)
+        {
+            // 指定された画像でBitmapを更新
+            image.ToBitmap(this.depthBitmap);
+
+            // PictureBoxの描画を要求
+            this.pictureBoxDepth.Invalidate();
         }
 
         /// <summary>
